40b52ca158788ec1af31e056f77414ca2ef482cb
Aaryan Sharda <aaryan.sharda31@gmail.com>
2025-07-20 19:41:37 +0530

    finished weather app

--- src/index.js
+++ src/index.js
@@ -3,32 +3,36 @@ import "./styles.css";
 const table = document.querySelector(".data");
 const loader = document.getElementById("loader");
 
-// fetch(
-//     "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/los%20angeles?unitGroup=us&key=UCQ9GSXYAVBEVXS7NQQ6VXZQ8&contentType=json",
-//     { mode: "cors" }
-// )
-//     .then((value) => {
-//         return value.json();
-//     })
-//     .then((value) => {
-//         console.log(value.currentConditions);
-//     });
+// // fetch(
+// //     "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/los%20angeles?unitGroup=us&key=UCQ9GSXYAVBEVXS7NQQ6VXZQ8&contentType=json",
+// //     { mode: "cors" }
+// // )
+// //     .then((value) => {
+// //         return value.json();
+// //     })
+// //     .then((value) => {
+// //         console.log(value.currentConditions);
+// //     });
 
 async function weatherInfo(value = "london") {
     table.textContent = " ";
+    if (table.classList.contains("error")) {
+        table.className = "data";
+    }
     loader.style.display = "block";
     let reqValue = value.replaceAll(" ", "%20");
     try {
         const obj = await fetch(
-            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${reqValue}?unitGroup=us&key=UCQ9GSXYAVBEVXS7NQQ6VXZQ8&contentType=json`,
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${reqValue}?unitGroup=us&elements=datetime%2CdatetimeEpoch%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Cdew%2Cconditions%2Cicon&key=UCQ9GSXYAVBEVXS7NQQ6VXZQ8&contentType=json`,
             { mode: "cors" }
         );
 
         const jsonValue = await obj.json();
+        console.log(jsonValue);
         const future = await jsonValue.days;
         let tableRow = document.createElement("tr");
         let header = document.createElement("thead");
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 9; i++) {
             let tableHeader = document.createElement("th");
             tableHeader.textContent = Object.keys(future[0])[i];
             tableRow.className = "header";
@@ -40,16 +44,24 @@ async function weatherInfo(value = "london") {
 
         future.forEach((value) => {
             let tableDataRow = document.createElement("tr");
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < 8; i++) {
                 let tableData = document.createElement("td");
                 tableData.textContent = Object.values(value)[i];
                 tableDataRow.appendChild(tableData);
             }
+            let icon = document.createElement("img");
+            import(
+                `./../images/1st Set - Color/${Object.values(value)[8]}.png`
+            ).then((result) => {
+                icon.src = result.default;
+            });
+            tableDataRow.appendChild(icon);
             table.appendChild(tableDataRow);
         });
     } catch {
         table.textContent =
             "We couldn't process your request. Please check that the name of the city is correct and try again";
+        table.classList.add("error");
     } finally {
         loader.style.display = "none";
     }
@@ -67,4 +79,8 @@ submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     weatherInfo(value);
     dialog.close();
+});
+
+import("./../images/1st Set - Color/clear-day.png").then((result) => {
+    console.log(result);
 });
